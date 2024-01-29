faf5850a1c075c4111a37c2281f708f3632b8567
1000338 <jvckigh@gmail.com>
2024-01-29 22:43:35 +0900

    feat: Adapter 패턴 추가 - 거니몬

---
+++ src/main/java/ch02/geonhee/example/_01/Main.java
@@ -0,0 +1,10 @@
+package ch02.geonhee.example._01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Print print = new PrintBanner("Hello");
+        print.printWeak();
+        print.printStrong();
+    }
+}
---
+++ src/main/java/ch02/geonhee/example/_01/Print.java
@@ -0,0 +1,8 @@
+package ch02.geonhee.example._01;
+
+public interface Print {
+
+    void printWeak();
+
+    void printStrong();
+}
