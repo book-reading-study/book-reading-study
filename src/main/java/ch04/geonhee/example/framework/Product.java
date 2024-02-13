eb43273575143d8644d86ff78092a97f5ba17892
1000338 <jvckigh@gmail.com>
2024-02-13 22:22:47 +0900

    feat: Factory Method 패턴 추가 - 거니몬

---
+++ src/main/java/ch04/geonhee/example/framework/Product.java
@@ -0,0 +1,6 @@
+package ch04.geonhee.example.framework;
+
+public abstract class Product {
+
+    public abstract void use();
+}
---
+++ src/main/java/ch04/geonhee/practice/framework/Product.java
@@ -0,0 +1,6 @@
+package ch04.geonhee.practice.framework;
+
+public abstract class Product {
+
+    public abstract void use();
+}
